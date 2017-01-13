d26e1c5ccd73e1f54e25be718566ff233ca9a214
Julio Saito <saitodisse@gmail.com>
2017-01-13 15:28:45 -0200

    add .vscode folder

--- Program.cs
+++ Program.cs
@@ -1,9 +1,18 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        List<String> arguments = new List<String>(args);
+        // Map = Select | Enumerable.Range(1, 10).Select(x => x + 2);
+        // Reduce = Aggregate | Enumerable.Range(1, 10).Aggregate(0, (acc, x) => acc + x);
+        // Filter = Where | Enumerable.Range(1, 10).Where(x => x % 2 == 0);
+
+        // Console.WriteLine("your args" + arguments.Join());
+
+        Console.WriteLine(arguments.Aggregate((i, j) => i + ", " + j));        
     }
 }
